f4a628c3aacdb280872640a864762b0741ebf20a
Mohamadi98 <mohamadinaena23@gmail.com>
2023-11-11 20:31:31 +0200

    first commit

---
+++ build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var person;
+person = 'Mohamadi';
+var printPersonName = function (person) {
+    return "the person name is: ".concat(person);
+};
+console.log(printPersonName(person));
