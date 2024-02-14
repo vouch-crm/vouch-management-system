96a0b177d7aaac4104316d4024a77035de04c309
Mohamadi98 <mohamadinaena23@gmail.com>
2024-02-14 21:16:01 +0200

    implementation for the employee training model and DAO

---
+++ src/models/employeeTraining.ts
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from 'mongoose';
+
+type EmployeeTrainingDocument = {
+    trainingTitle: string,
+    empID: string,
+    trainingType: string,
+    TrainingCategory?: string,
+    status?: string,
+    startOn?: Date,
+    endOn?: Date,
+    reminderEmail?: string,
+    provider?: string,
+    currency?: string,
+    cost?: number,
+    outcome?: string,
+    notes?: string
+}
+
+const employeeTrainingSchema = new Schema({
+    empID: {
+        type: Schema.Types.ObjectId,
+        ref: 'Employee',
+        required: true
+    },
+    trainingTitle: String,
+    trainingType: String,
+    TrainingCategory: String,
+    status: String,
+    startOn: Date,
+    endOn: Date,
+    reminderEmail: String,
+    provider: String,
+    currency: String,
+    cost: Number,
+    outcome: String,
+    notes: String
+});
+
+const EmployeeTrainingAgent = mongoose.model<EmployeeTrainingDocument>('SalaryUpdates',
+    employeeTrainingSchema);
+
+
+export { EmployeeTrainingDocument, EmployeeTrainingAgent }
