47c67ede46974c3ced78bcbbc1f499395033b4fa
Ahmed Ali <thunderstruck772@gmail.com>
2024-03-10 04:39:19 +0200

    ran build command for render

--- build/controllers/TaskController.js
+++ build/controllers/TaskController.js
@@ -5,14 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const taskModel_1 = require("../models/taskModel");
-const jobModel_1 = require("../models/jobModel");
 const taskRouter = express_1.default.Router();
 const createTask = async (req, res) => {
     try {
         const reqBody = req.body;
-        if (!reqBody.jobID) {
-            const newJob = await jobModel_1.jobAgent.create({});
-        }
         const newTask = await taskModel_1.TaskAgent.create(reqBody);
         res.status(201).json(newTask);
     }
